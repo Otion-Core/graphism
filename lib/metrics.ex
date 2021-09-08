a08d1f7a24db9bffc350af4f0b949aa231033d81
Pedro Gutiérrez <pedro.gutierrez@bite.social>
2021-09-09 01:04:39 +0200

    fix: allow for extra prom_ex plugins

--- lib/metrics.ex
+++ lib/metrics.ex
@@ -12,37 +12,66 @@ defmodule Graphism.Metrics do
         use PromEx, otp_app: unquote(otp_app)
         alias PromEx.Plugins
 
-        @absinthe_metrics_prefix [:graphism, :absinthe]
-        @ecto_metrics_prefix [:graphism, :ecto]
-        @beam_metrics_prefix [:graphism, :beam]
-        @application_metrics_prefix [:graphism, :application]
-        @prom_ex_metrics_prefix [:graphism, :prom_ex]
+        @extra_plugins Application.get_env(unquote(otp_app), __MODULE__)[:plugins] || []
+        @otp_app_metric_prefix unquote("#{otp_app}_metric_prefix" |> String.to_atom())
+        @otp_app unquote(otp_app)
+        @extra_dashboard_assigns [
+          {@otp_app, "#{@otp_app}"},
+          {@otp_app_metric_prefix, "#{@otp_app}"}
+        ]
+
+        @extra_dashboards @extra_plugins
+                          |> Enum.map(fn {_, opts} ->
+                            case opts[:dashboard] do
+                              nil ->
+                                nil
+
+                              dashboard ->
+                                {@otp_app, dashboard,
+                                 title:
+                                   dashboard
+                                   |> String.split("/")
+                                   |> List.last()
+                                   |> String.split(".")
+                                   |> List.first()
+                                   |> String.capitalize()}
+                            end
+                          end)
+                          |> Enum.reject(&is_nil/1)
+
+        @absinthe_metric_prefix [:graphism, :absinthe]
+        @ecto_metric_prefix [:graphism, :ecto]
+        @beam_metric_prefix [:graphism, :beam]
+        @application_metric_prefix [:graphism, :application]
+        @prom_ex_metric_prefix [:graphism, :prom_ex]
 
         @impl true
         def plugins do
           [
             {Plugins.Absinthe,
              absinthe_entrypoint_tag_value_fun: &entrypoint_tag_value/1,
-             metric_prefix: @absinthe_metrics_prefix,
+             metric_prefix: @absinthe_metric_prefix,
              ignored_entrypoints: [:__schema]},
-            {Plugins.Ecto, metric_prefix: @ecto_metrics_prefix},
-            {Plugins.Beam, metric_prefix: @beam_metrics_prefix},
-            {Plugins.Application, metric_prefix: @application_metrics_prefix},
-            {Plugins.PromEx, metric_prefix: @prom_ex_metrics_prefix}
-          ]
+            {Plugins.Ecto, metric_prefix: @ecto_metric_prefix},
+            {Plugins.Beam, metric_prefix: @beam_metric_prefix},
+            {Plugins.Application, metric_prefix: @application_metric_prefix},
+            {Plugins.PromEx, metric_prefix: @prom_ex_metric_prefix}
+          ] ++
+            Enum.map(@extra_plugins, fn {module, opts} ->
+              {module, metric_prefix: Keyword.fetch!(opts, :metric_prefix)}
+            end)
         end
 
         @impl true
         def dashboard_assigns do
           [
             datasource_id: unquote(datasource_id),
-            plug_absinthe_metric_prefix: Enum.join(@absinthe_metrics_prefix, "_"),
-            ecto_metric_prefix: Enum.join(@ecto_metrics_prefix, "_"),
-            beam_metric_prefix: Enum.join(@beam_metrics_prefix, "_"),
-            application_metric_prefix: Enum.join(@application_metrics_prefix, "_"),
-            prom_ex_metric_prefix: Enum.join(@prom_ex_metrics_prefix, "_"),
-            otp_app: "#{unquote(otp_app)}"
-          ]
+            absinthe_metric_prefix: Enum.join(@absinthe_metric_prefix, "_"),
+            ecto_metric_prefix: Enum.join(@ecto_metric_prefix, "_"),
+            beam_metric_prefix: Enum.join(@beam_metric_prefix, "_"),
+            application_metric_prefix: Enum.join(@application_metric_prefix, "_"),
+            prom_ex_metric_prefix: Enum.join(@prom_ex_metric_prefix, "_")
+          ] ++ @extra_dashboard_assigns
         end
 
         @impl true
@@ -52,7 +81,8 @@ defmodule Graphism.Metrics do
             {:prom_ex, "ecto.json", title: "Ecto"},
             {:prom_ex, "beam.json", title: "Beam"},
             {:prom_ex, "application.json", title: "Application"}
-          ]
+          ] ++
+            @extra_dashboards
         end
 
         defp entrypoint_tag_value(%{selections: [%{selections: [op | _]} = e | _]}) do
