8524719c72e004b058077c921138a84341122cf7
Pedro Gutiérrez <pedrogutierrez@mac.com>
2024-09-20 12:17:35 +0200

    Release v0.16.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Graphism.MixProject do
   def project do
     [
       app: :graphism,
-      version: "0.15.0",
+      version: "0.16.0",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
